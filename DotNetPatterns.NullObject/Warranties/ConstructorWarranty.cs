f44e2d1cd0da5cc234f60590b559bc99e18e7ec6
thomas.halin <thomas.halin.th@gmail.com>
2020-06-06 11:06:07 +0200

    Added null object pattern

---
+++ DotNetPatterns.NullObject/Warranties/ConstructorWarranty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPatterns.NullObject.Warranties
+{
+    public class ConstructorWarranty : Warranty
+    {
+        public override void Claim()
+        {
+            Console.WriteLine("Claiming Constructor Warranty");
+        }
+    }
+}
