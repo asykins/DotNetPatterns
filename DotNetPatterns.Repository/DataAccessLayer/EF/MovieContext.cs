5738e3f6fa29f777848efe7e80b78d44bb597a6f
thomas.halin <thomas.halin.th@gmail.com>
2020-06-17 00:06:45 +0200

    Modified repository pattern, abstract classes for generic repo using predicates with CSVs and EF

---
+++ DotNetPatterns.Repository/DataAccessLayer/EF/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using DotNetPatterns.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPatterns.Repository.DataAccessLayer.EF
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions options)
+            : base(options) { }
+
+        DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ DotNetPatterns.Repository/Repositories/EntityFrameworkRepository.cs
@@ -0,0 +1,28 @@
+﻿using DotNetPatterns.Repository.DataAccessLayer.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DotNetPatterns.Repository.Repositories
+{
+    public abstract class EntityFrameworkRepository<T> : IRepository<T>
+        where T: class
+    {
+        private readonly MovieContext _context;
+
+        public EntityFrameworkRepository(MovieContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        private IQueryable<T> AggregateWhereExpressions
+            (IQueryable<T> query, Expression<Func<T, bool>>[] predicates)
+            => predicates.Aggregate(query, (currentQuery, predicate) => currentQuery.Where(predicate));
+
+        public IReadOnlyList<T> Find(params Expression<Func<T, bool>>[] predicates)
+        {
+            return AggregateWhereExpressions(_context.Set<T>(), predicates).ToList();
+        }
+    }
+}
